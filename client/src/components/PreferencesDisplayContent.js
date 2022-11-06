1c58ee841e30060d93850a8c6ba8cd7efa269c11
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-11-06 15:34:30 +0200

    Fixes all preference radios having same name

--- client/src/components/PreferencesDisplayContent.js
+++ client/src/components/PreferencesDisplayContent.js
@@ -3,8 +3,8 @@ import PreferencesInputGroup from "./PreferencesInputGroup"
 export default function PreferencesDisplayContent(){
     return(
         <div>
-            <PreferencesInputGroup setting="Two charts side by side"/>
-            <PreferencesInputGroup setting="Temperature anomaly chart"/>
+            <PreferencesInputGroup setting="Two charts side by side" name="settingOneRadios"/>
+            <PreferencesInputGroup setting="Temperature anomaly chart" name="settingTwoRadios"/>
         </div>
     )
 }
