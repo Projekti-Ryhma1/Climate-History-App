250ceb913a0b1196e08e9a16537279ee8c647050
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-11-06 15:34:30 +0200

    Fixes all preference radios having same name

--- client/src/components/PreferencesInputGroup.js
+++ client/src/components/PreferencesInputGroup.js
@@ -5,13 +5,13 @@ export default function PreferencesInputGroup(props){
         <div class="div-centered">
             <div class="row my-5">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked/>
+                    <input class="form-check-input" type="radio" name={props.name} id="exampleRadios1" value="option1" checked/>
                     <label class="form-check-label" for="exampleRadios1">
                         Enable {props.setting}
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2"/>
+                    <input class="form-check-input" type="radio" name={props.name} id="exampleRadios2" value="option2"/>
                     <label class="form-check-label" for="exampleRadios2">
                         Disable {props.setting}
                     </label>
