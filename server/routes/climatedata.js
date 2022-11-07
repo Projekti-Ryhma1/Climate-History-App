4fba68aff4b23109175f97ddcf614540bd96c750
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-07 16:10:35 +0200

    create climatedata router and path for it

---
+++ server/routes/climatedata.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const connection = require("./database");
+
+router.get("/global_monthly", async (req, res) => {
+  const query = "SELECT * FROM global_monthly";
+  connection.query(query, (error, result) => {
+    if (error) throw error;
+    res.json(result);
+  });
+});
+module.exports = router;
