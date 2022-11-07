f180aa6b11f0c0b0786fff1b1dff73e1675d01b2
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-07 17:53:33 +0200

    rename LineChart.js module to ClimateLineChart.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import ClimateLineChart from './modules/ClimateLineChart';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
+
+      <ClimateLineChart/>
     </div>
   );
 }
---
+++ client/src/modules/ClimateLineChart.js
@@ -0,0 +1,13 @@
+import { LineChart } from "recharts"
+
+export default function ClimateLineChart(){
+
+
+
+    return(
+        <di>
+            <LineChart width={400} height={400}></LineChart>
+
+        </di>
+    )
+} 
