781aafc5c74d8292bc726cf44815b923e68136d7
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-07 16:14:56 +0200

    fix path to database.js in climatedata.js

--- server/routes/climatedata.js
+++ server/routes/climatedata.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const connection = require("./database");
+const connection = require("../database");
 
 router.get("/global_monthly", async (req, res) => {
   const query = "SELECT * FROM global_monthly";
