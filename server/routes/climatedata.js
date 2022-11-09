f2497d5bbc69d66bbe6180c307a5ec3d1b824c6d
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-09 11:54:57 +0200

    add new route for getting northern hemisphere monthly data

--- server/routes/climatedata.js
+++ server/routes/climatedata.js
@@ -9,5 +9,11 @@ router.get("/global_monthly", async (req, res) => {
     res.json(result);
   });
 });
-
+router.get("/northern_hemisphere_monthly", async (req, res) => {
+  const query = "SELECT * FROM northern_hemisphere_monthly";
+  connection.query(query, (error, result) => {
+    if (error) throw error;
+    res.json(result);
+  });
+});
 module.exports = router;
