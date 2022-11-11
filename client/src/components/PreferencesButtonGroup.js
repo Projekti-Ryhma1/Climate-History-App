301d82e52f6ff90fc0f977c6f561a3b9d11026b8
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-11-11 14:44:37 +0200

    Updated dependencies and changed preferences input group to switches

---
+++ client/src/components/PreferencesButtonGroup.js
@@ -0,0 +1,10 @@
+import Button from 'react-bootstrap/Button'
+
+export default function PreferencesButtonGroup(props){
+    return(
+        <div className="div-button-group">
+            <Button>Exit button</Button>
+            <Button>Save button</Button>
+        </div>
+    );
+}
---
+++ client/src/components/PreferencesSwitchGroup.js
@@ -0,0 +1,42 @@
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+export default function PreferencesSwitchGroup(props){
+    return(
+        <div className="div-switch-group">
+                <Form className="mb-3">
+                    <Row>
+                        <Col>
+                            <Form.Check 
+                                defaultChecked={props.checked}
+                                type="switch"
+                                id={props.id+"enabled"}
+                                label={props.label}
+                                name={props.name}
+                            />
+                        </Col>
+{/*                         <Col>
+                            <Button variant="primary" onClick={() =>consolePrint(props.id)} data={props.id} id="1235">
+                                    Click me
+                            </Button>
+                        </Col> */}
+                    </Row>
+                </Form>
+        </div>
+    )
+}
+
+/* function consolePrint(test){
+    //const $ = document.querySelectorAll('input[type="radio"]')
+    const $ = document.querySelectorAll('input[id="'+test+'enabled"]')
+
+    for(var i = 0; i < $.length; i++)
+    {
+        if($[i].checked)
+        {
+            console.log("Checked Radio Found!");
+            console.log(document.getElementById("1235").data)
+        }
+    }
+} */
