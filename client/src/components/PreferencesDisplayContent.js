1f7196f7fe571c75f90e8d7c3d3c30f8eab295b3
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-11-11 14:44:37 +0200

    Updated dependencies and changed preferences input group to switches

--- client/src/components/PreferencesDisplayContent.js
+++ client/src/components/PreferencesDisplayContent.js
@@ -1,10 +1,17 @@
-import PreferencesInputGroup from "./PreferencesInputGroup"
+import PreferencesSwitchGroup from "./PreferencesSwitchGroup"
+import PrefenrecesButtonGroup from "./PreferencesButtonGroup"
 
 export default function PreferencesDisplayContent(){
+    const checked = true;
     return(
-        <div>
-            <PreferencesInputGroup setting="Two charts side by side" name="settingOneRadios"/>
-            <PreferencesInputGroup setting="Temperature anomaly chart" name="settingTwoRadios"/>
+        <div className="div-centered">
+            <PreferencesSwitchGroup label="Charts side by side" name="settingOneRadios" checked={checked} id="1"/>
+            <PreferencesSwitchGroup label="Anomaly chart" name="settingTwoRadios" checked={checked} id="2"/>
+            <PreferencesSwitchGroup label="chart 2 toggle" name="settingThreeRadios" id="3"/>
+            <PreferencesSwitchGroup label="Chart 3 toggle" name="settingFourRadios" id="4"/>
+            <PreferencesSwitchGroup label="chart 4 toggle" name="settingFiveRadios" checked={checked} id="5"/>
+            <PreferencesSwitchGroup label="chart 5 toggle" name="settingSixRadios" id="6"/>
+            <PrefenrecesButtonGroup/>
         </div>
     )
 }
