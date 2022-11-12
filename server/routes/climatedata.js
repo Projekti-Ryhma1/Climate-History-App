cdea6ecbded3de6f40ac8b9100f546640f352b3a
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-12 14:04:32 +0200

    updated get functions and put them inside try catch

--- server/routes/climatedata.js
+++ server/routes/climatedata.js
@@ -1,26 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const connection = require("../database");
+const database = require("../models/climatedata_model");
 
 router.get("/global_monthly", async (req, res) => {
-  const query = "SELECT * FROM global_monthly";
-  connection.query(query, (error, result) => {
-    if (error) throw error;
-    res.json(result);
-  });
+  try {
+    res.status(200).json(await database.getGlobalTempAnomaly());
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
-router.get("/northern_hemisphere_monthly", async (req, res) => {
-  const query = "SELECT * FROM northern_hemisphere_monthly";
-  connection.query(query, (error, result) => {
-    if (error) throw error;
-    res.json(result);
-  });
-});
+
 router.get("/northern_hemisphere_2000_year", async (req, res) => {
-  const query = "SELECT * FROM northern_hemisphere_2000_year_temperature_reconstruction";
-  connection.query(query, (error, result) => {
-    if (error) throw error;
-    res.json(result);
-  });
+  try {
+    res.status(200).json(await database.getNorthHemis2000YearTemp());
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
 });
 module.exports = router;
