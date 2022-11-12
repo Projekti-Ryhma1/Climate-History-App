cb58c711d6814c71597a527b467169a752dcda13
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-12 12:25:37 +0200

    created function to query global temp anomalies

--- server/models/climatedata_model.js
+++ server/models/climatedata_model.js
@@ -1,2 +1,13 @@
 const database = require("../database")
 
+async function getGlobalTempAnomaly(){
+    const query = "SELECT * FROM global_temp_anomaly";
+    database.query(query,(error, result)=>{
+        if(error) throw error;
+        return result;
+    })
+}
+
+module.exports = { 
+    getGlobalTempAnomaly
+}
