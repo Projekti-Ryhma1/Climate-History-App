df7d26c54fb809f33cd24a673edc08d91c308d4e
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-12 14:03:12 +0200

    created db query promises

--- server/models/climatedata_model.js
+++ server/models/climatedata_model.js
@@ -1,13 +1,30 @@
-const database = require("../database")
+const database = require("../database");
 
-async function getGlobalTempAnomaly(){
-    const query = "SELECT * FROM global_temp_anomaly";
-    database.query(query,(error, result)=>{
-        if(error) throw error;
-        return result;
-    })
-}
+getGlobalTempAnomaly = () => {
+  const query = "SELECT * FROM global_temp_anomaly";
+  return new Promise((resolve, reject) => {
+    database.query(query, (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(result);
+    });
+  });
+};
+getNorthHemis2000YearTemp = () => {
+  const query =
+    "SELECT * FROM northern_hemisphere_2000_year_temperature_reconstruction";
+  return new Promise((resolve, reject) => {
+    database.query(query, (error, result) => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve(result);
+    });
+  });
+};
 
-module.exports = { 
-    getGlobalTempAnomaly
-}
+module.exports = {
+  getGlobalTempAnomaly,
+  getNorthHemis2000YearTemp,
+};
