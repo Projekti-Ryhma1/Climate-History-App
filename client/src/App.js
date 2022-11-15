551055d6a96190227553034033f446578915c678
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-11-15 10:11:03 +0200

    Using Axios to send http calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import ClimateLineChart from './modules/ClimateLineChart';
+import{ Routes, Route } from "react-router-dom";
+import Preferences from './pages/Preferences';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      {/* <h2>Hello world</h2>
 
-      <ClimateLineChart/>
+      <ClimateLineChart/> */}
+      
+      <div>
+        <Routes>
+          <Route path="/" element={<Preferences/>} />
+        </Routes>
+      </div>
+
     </div>
+
+    
   );
 }
 
