328db8ae414a35ab4805e1eaf0301477cb83b2fd
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-16 14:18:49 +0200

    added colour to sectors & show sector name

--- client/src/modules/EmissionPieChart.js
+++ client/src/modules/EmissionPieChart.js
@@ -1,22 +1,42 @@
-import { Pie, PieChart } from "recharts";
+import { Cell, Pie, PieChart, Tooltip } from "recharts";
+import "./EmissionPieChart.css"
 
 export default function EmissionPieChart() {
+  const COLORS = ["#ffff00", "#FF8042", "#996633", "#009900"];
   const renderPie = (
     <>
-    <p>Emission by Sector</p>
+      <p>Emission by Sector</p>
       <PieChart width={400} height={400}>
         <Pie data={data3} />
       </PieChart>
     </>
   );
-  return ( <div>
-    <p>Emission by Sector</p>
-      <PieChart width={400} height={400}>
-        <Pie data={data3} dataKey="emissions" />
-     </PieChart>
-  </div>)
+  const renderLabel = (entry) => {
+    return entry.Sector
+  };
+
+  return (
+    <div className="container-chart">
+      <p>Emission by Sector</p>
+      <PieChart width={800} height={400}>
+        <Tooltip></Tooltip>
+        <Pie
+          data={data3}
+          dataKey="emissions"
+          cx="50%"
+          cy="50%"
+          label={renderLabel}
+          paddingAngle="1"
+        >
+          {data3.map((entry, index) => (
+            <Cell fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </div>
+  );
 }
- 
+
 const data3 = [
   {
     Sector: "Energy",
@@ -31,7 +51,7 @@ const data3 = [
     emissions: 3.2,
   },
   {
-    Sector: "Agriculture, Forestry & Land Use (AFOLU)",
+    Sector: "Agriculture, Forestry & Land Use",
     emissions: 18.4,
   },
 ];
