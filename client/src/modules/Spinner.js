b980dd582a34767a6e288d0a2a88f5586d796f4a
Jarno <jarwes90@gmail.com>
2022-11-16 10:53:31 +0200

    hard reset

--- client/src/modules/Spinner.js
+++
@@ -1,8 +0,0 @@
-import "./Spinner.css"
-export default function Spinner() {
-    return (
-      <div className="spinner-container">
-        <div className="loading-spinner"></div>
-      </div>
-    );
-  }
