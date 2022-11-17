3b094f7145f1195b4233eb36a320fb71c72f5661
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-17 12:13:52 +0200

    highlight active sector

--- client/src/modules/EmissionPieChart.js
+++ client/src/modules/EmissionPieChart.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { LabelList, Cell, Pie, PieChart, Tooltip } from "recharts";
+import { useState, useCallback } from "react";
+import { LabelList, Cell, Pie, PieChart, Sector } from "recharts";
 import "./EmissionPieChart.css";
 
 /** TODO
@@ -11,8 +11,9 @@ import "./EmissionPieChart.css";
 
 export default function EmissionPieChart() {
   const COLORS = ["#ffff00", "#FF8042", "#996633", "#009900"];
-  const [isLoading,setIsLoading] = useState(true)
-  const [data3, setdata3] = useState([])
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [data4, setdata4] = useState([]);
 
   const renderLabel = (entry) => {
     return entry.Sector;
@@ -21,47 +22,61 @@ export default function EmissionPieChart() {
     let percentageToString = data3.emissions;
     return percentageToString.toFixed(1).replace(".", ",").toString() + "%";
   };
-  const renderPie = (
-    <>
-      <p>Emission by Sector</p>
-      <PieChart width={800} height={500}>
-        <Pie
-          data={data3}
-          dataKey="emissions"
-          cx="50%"
-          cy="50%"
-          label={renderLabel}
-          paddingAngle="1"
-        >
-          {data3.map((entry, index) => (
-            <Cell fill={COLORS[index % COLORS.length]} />
-          ))}
-          <LabelList
-            fill="#4d4d4d" // Percentage colour
-            dataKey={renderPercentage}
-            position="inside"
-            angle="0"
-            stroke="none" // Border of letters
-            className="label-percentage"
-          />
-        </Pie>
-      </PieChart>
-    </>
+  const renderActiveShape = (props) => {
+    const {
+      cx,
+      cy,
+      midAngle,
+      innerRadius,
+      outerRadius,
+      startAngle,
+      endAngle,
+      fill,
+      payload,
+      percent,
+      value,
+    } = props;
+
+    return (
+      <>
+        <g></g>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius + 10}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+      </>
+    );
+  };
+  const onPieEnter = useCallback(
+    (_, index) => {
+      console.log(index);
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
   );
+  const renderPie = <></>;
   return (
     <div className="container-chart">
       <p>Emission by Sector</p>
       <PieChart width={800} height={500}>
         <Pie
+          activeIndex={activeIndex}
+          activeShape={renderActiveShape}
           data={data3}
           dataKey="emissions"
           cx="50%"
           cy="50%"
           label={renderLabel}
           paddingAngle="1"
+          onMouseEnter={onPieEnter}
         >
           {data3.map((entry, index) => (
-            <Cell fill={COLORS[index % COLORS.length]} />
+            <Cell key={entry.Sector} fill={COLORS[index % COLORS.length]} />
           ))}
           <LabelList
             fill="#4d4d4d" // Percentage colour
