7a9c275fd23d6f93cea956c2a02b619ec3d86de3
Jarno <jarwes90@gmail.com>
2022-11-21 13:43:40 +0200

    updates

--- server/models/login_model.js
+++ server/models/login_model.js
@@ -1,20 +1,4 @@
 const db = require('../database');
-/*
-const login = {
-  getPasswordByName: function(username, callback) {
-    return db.query('select password from users where username=?', [username], callback);
-  },
-  getPasswordByEmail: function(email, callback) {
-    return db.query('select password from users where email=?', [email], callback);
-  },
-
-  getUserByName: function(username, callback) {
-    return db.query('select * from users where username=?', [username], callback);
-  },
-  getUserByEmail: function(email, callback) {
-    return db.query('select * from users where email=?', [email], callback);
-  },
-};*/
 
 userLogin = (username) => {
   const query = "select password from users where username=?";
