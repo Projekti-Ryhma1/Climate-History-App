a3949317fc6b9a6caca84f4d7f6b6ea1287bff4d
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-28 11:18:32 +0200

    working navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import AtmosphericCO2LineChart from './modules/AtmosphericCO2LineChart';
 import ClimateLineChart from './modules/ClimateLineChart';
 import EmissionPieChart from './modules/EmissionPieChart';
@@ -9,6 +10,7 @@ import StackedLineChart from './modules/StackedLineChart';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h2>Hello world</h2>
       <AtmosphericCO2LineChart/>
        <EmissionPieChart/>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,29 +1,20 @@
-import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 export default function NavBar() {
     return (
-        <nav class="navbar navbar-expand navbar-dark bg-dark" aria-label="Second navbar example">
-            <div class="container-fluid">
-               <Link className="navbar-brand" to="/" >Climate data</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarsExample02">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li class="nav-item">
-                        <Link className="nav-link" to="/">PlaceHolder</Link>
-                        </li>
-                        <li class="nav-item">
-                        <Link className="nav-link" to="/">PlaceHolder</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Climate Data</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Temperature & CO2 Concentrations</Nav.Link>
+            <Nav.Link href="#features">Emissions</Nav.Link>
+            <Nav.Link href="#pricing">Custom Page</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
     )
 }
