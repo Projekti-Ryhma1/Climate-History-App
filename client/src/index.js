c952258de51e9e5e72a4a6d486a178e6797db83f
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-28 11:12:14 +0200

    import css

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
