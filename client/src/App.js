cb5ce916d693f9931ace00635694cd4001ef18df
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-11-28 10:41:37 +0200

    add chart to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AtmosphericCO2LineChart from './modules/AtmosphericCO2LineChart';
 import ClimateLineChart from './modules/ClimateLineChart';
 import EmissionPieChart from './modules/EmissionPieChart';
 import StackedLineChart from './modules/StackedLineChart';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
+      <AtmosphericCO2LineChart/>
        <EmissionPieChart/>
        <ClimateLineChart/>
        <StackedLineChart/>
