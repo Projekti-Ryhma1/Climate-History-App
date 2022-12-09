9c556861347f86d5408ad2ce6432bdfaa9f26df9
Jarno <jarwes90@gmail.com>
2022-12-09 12:03:30 +0200

    Navbar mobile view added and first version of responsive charts

--- client/src/components/EvoGlobalTempBiaxiallinechart.js
+++ client/src/components/EvoGlobalTempBiaxiallinechart.js
@@ -1,7 +1,7 @@
 import "./EvoGlobalTempBiaxiallinechart.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { Label, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
+import { ResponsiveContainer, Label, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 import Spinner from "./Spinner";
 
 export default function EvoGlobalTempBiaxiallinechart() {
@@ -29,32 +29,49 @@ export default function EvoGlobalTempBiaxiallinechart() {
         }, 500);
     }, []);
 
+    async function checkIfWindowScale() {
+        const width = window.innerWidth;
+        const maxWidth = 960;
+
+        if((width>maxWidth)) {
+            console.log("Max width");
+            return 960;
+        } else {
+            console.log("Scale width");
+            return window.innerWidth;
+        }
+
+
+    }
+
     const renderChart = (<>
         <p id="headline">Evolution of global temperature over the past two million years</p>
-        <LineChart id="lineChart" width={960} height={420} data={evoData}
-            margin={{ top: 10, right: 30, left: 20, bottom: 10 }}>
-            <XAxis dataKey="time (kyr BP)" domain={["auto", "auto"]} reversed="true" interval="preserveStartEnd">
-                <Label value={"Time (ka)"} offset={-10} position="insideBottom"/>
-            </XAxis>
-            <YAxis hide="false" tickLine="false"/>
-            <YAxis id="temp" yAxisId={1} domain={["auto", "auto"]} padding={{ top: 190}} tickLine="false"  orientation="right" />
-            <YAxis yAxisId={2} domain={["auto", "auto"]} padding={{ bottom: 190 }} tickLine="false"  orientation="left"/>
-            <Tooltip />
-            <Legend verticalAlign="top"/>
-            <Line type="monotone" dataKey="50 %" name="Change in global temperature (°C)" stroke="#8884d8" dot={false} yAxisId={1}/>
-            <Line type="monotone" dataKey="carbon dioxide (ppm)" name="carbon dioxide (ppm)" stroke="#82ca9d" dot={false}yAxisId={2}/>
-        </LineChart>
+        <ResponsiveContainer width={'100%'} height={420}>
+            <LineChart id="lineChart" width={960} height={420} data={evoData}
+                margin={{ top: 10, right: 30, left: 20, bottom: 10 }}>
+                <XAxis dataKey="time (kyr BP)" domain={["auto", "auto"]} reversed="true" interval="preserveStartEnd">
+                    <Label value={"Time (ka)"} offset={-10} position="insideBottom" />
+                </XAxis>
+                <YAxis hide="false" tickLine="false" />
+                <YAxis id="temp" yAxisId={1} domain={["auto", "auto"]} padding={{ top: 190 }} tickLine="false" orientation="right" />
+                <YAxis yAxisId={2} domain={["auto", "auto"]} padding={{ bottom: 190 }} tickLine="false" orientation="left" />
+                <Tooltip />
+                <Legend verticalAlign="top" />
+                <Line type="monotone" dataKey="50 %" name="Change in global temperature (°C)" stroke="#8884d8" dot={false} yAxisId={1} />
+                <Line type="monotone" dataKey="carbon dioxide (ppm)" name="carbon dioxide (ppm)" stroke="#82ca9d" dot={false} yAxisId={2} />
+            </LineChart>
+        </ResponsiveContainer>
         <div>
             <p id="description">
-            Shows the global temperature over the past 2 million years estimated from a database of over 20,000 sea
-            surface temperature point reconstructions.
+                Shows the global temperature over the past 2 million years estimated from a database of over 20,000 sea
+                surface temperature point reconstructions.
             </p>
         </div>
     </>);
 
     return (
         <div className="biaxialline-chart">
-            { isLoading ? <Spinner /> : renderChart }
+            {isLoading ? <Spinner /> : renderChart}
         </div>
     );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,38 +1,106 @@
+import './NavBar.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useCookies } from 'react-cookie';
 import jwtDecode from 'jwt-decode';
+import Row from 'react-bootstrap/esm/Row';
+import { useEffect, useState } from 'react';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+const maxWindowWidth = 830;
 
 export default function NavBar() {
   const [cookies] = useCookies(['token']);
-
-  let userRoutes = <>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            <Nav.Link href="/Login">Login</Nav.Link>
-  </>
-  if(cookies.token) {
-    let decodedToken = jwtDecode(cookies.token);
-    let username = decodedToken.username;
-
-    userRoutes = <>
-            <Nav.Link href="/logout">Logout ( {username} )</Nav.Link>
-            <Nav.Link href="/preferences">Preferences</Nav.Link>
-    </>
+  const [isMobileView, setMobileSize] = useState(window.innerWidth < maxWindowWidth);
+  let username = "";
+  if (cookies.token) {
+    username = jwtDecode(cookies.token).username;
   }
 
-    return (
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="/">Climate Data</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Temperature & CO2 Concentrations</Nav.Link>
-            <Nav.Link href="/emissions">Emissions</Nav.Link>
-            <Nav.Link href="#pricing">Custom Page</Nav.Link>
-            { userRoutes }
+  useEffect(() => {
+    window.addEventListener('resize', function () {
+      const windowSize = window.innerWidth;
+      setMobileSize(windowSize < maxWindowWidth);
+    });
+  }, [])
+
+  function checkRoutes() {
+    let result;
+    if (cookies.token) { // User is logged in
+      if (!isMobileView) {
+        result =
+          <Nav className="lt-auto" variant="dark" >
+            <Navbar.Toggle aria-controls="navbar-normal" />
+            <Navbar.Collapse id="navbar-normal" >
+              <NavDropdown title={"User ( " + username + " )"}>
+                <ListGroup>
+                  <ListGroup.Item action href="/preferences">Preferences</ListGroup.Item>
+                  <ListGroup.Item action href="/logout">Logout</ListGroup.Item>
+                </ListGroup>
+              </NavDropdown>
+            </Navbar.Collapse>
           </Nav>
+      } else {
+        result =
+          <ListGroup>
+            <ListGroup.Item action href="/preferences">Preferences</ListGroup.Item>
+            <ListGroup.Item action href="/logout">Logout</ListGroup.Item>
+          </ListGroup>
+      }
+    } else { // User is not logged in
+      if (isMobileView) {
+        result =
+          <ListGroup>
+            <ListGroup.Item action href="/signup">Signup</ListGroup.Item>
+            <ListGroup.Item action href="/Login">Login</ListGroup.Item>
+          </ListGroup>
+      } else {
+        result =
+        <>
+          <Nav.Link href="/signup">Signup</Nav.Link>
+          <Nav.Link href="/Login">Login</Nav.Link>
+        </>
+      }
+    }
+
+
+    return result;
+  }
+
+
+  if (!isMobileView) { // Desktop window
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Row>
+            <Nav className="me-auto">
+              <Navbar.Brand href="/" >Climate Data</Navbar.Brand>
+              <Nav.Link href="/">Temperature & CO2 Concentrations</Nav.Link>
+              <Nav.Link href="/emissions">Emissions</Nav.Link>
+              {checkRoutes()}
+            </Nav>
+          </Row>
         </Container>
       </Navbar>
-
     )
+  }
+  else { //Mobile window
+    return (
+      <Navbar expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/" >Climate Data</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-mobile" />
+          <Navbar.Collapse id="navbar-mobile">
+            <ListGroup>
+              <ListGroup.Item action href="/">Temperature & CO2 Concentrations</ListGroup.Item>
+              <ListGroup.Item action href="/emissions">Emissions</ListGroup.Item>
+            </ListGroup>
+            {checkRoutes()}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  }
 }
