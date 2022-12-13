41afb8c7ac8eb46e4878d2b89a3d6ddb01333a91
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-12-14 01:27:21 +0200

    Added e2e testing with cypress for user creation, login and deletion

---
+++ client/cypress/e2e/account.cy.js
@@ -0,0 +1,22 @@
+describe('create user, login, delete user', () => {
+  it('user can create account', () => {
+      cy.visit('localhost:3000/')
+      //Signup
+      cy.findByRole('link', { name: /signup/i }).click()
+      cy.get('[id=userName]').type('1234')
+      cy.get('[id=userPassword]').type('1234')
+      cy.get('[id=userEmail]').type('1234@1234.com')
+      cy.findByRole('button', {  name: /create/i}).click()
+      //Login
+      cy.visit('localhost:3000/login')
+      cy.get('[id=userName]').type('1234')
+      cy.get('[id=userPassword]').type('1234')
+      cy.findByRole('button', {  name: /login/i}).click()
+      //Delete user
+      cy.findByRole('link', {  name: /preferences/i}).click()
+      cy.findByRole('button', {  name: /delete user/i}).click()
+      cy.findByRole('dialog').within(($form) => {
+      cy.findByRole('button', {  name: /delete user/i}).click()
+      })
+  })
+})
