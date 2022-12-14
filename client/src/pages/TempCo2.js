bae2c3bd3e469b0973706d42457f927148757d54
Jarno <jarwes90@gmail.com>
2022-12-14 10:13:46 +0200

    Fixes #48

--- client/src/pages/TempCo2.js
+++ client/src/pages/TempCo2.js
@@ -6,6 +6,7 @@ import VostokIceLineChart from "../components/VostokIceLineChart";
 import EvoGlobalTempBiaxiallinechart from "../components/EvoGlobalTempBiaxiallinechart";
 
 // maxWindowWidth: Horizontal window width limit for more mobile friendly solution
+
 export default function TempCo2(props){
     return(
         <div style={{marginTop: '10px', marginBottom: '50px'}}>
